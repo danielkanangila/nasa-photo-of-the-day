0437a7299b94231fdf2963ae33d88f68fd6f9f84
Daniel Kanangila <daniel.kanangila@gmail.com>
2020-01-07 00:07:51 -0500

    Added NavBar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import Container from '@material-ui/core/Container';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles'
+import NavBar from './Components/NavBar';
 import "./App.css";
 
 function App() {
+
+  const [ nDate, setNDate ] = useState('1988-06-06');
+
+  const theme = createMuiTheme({
+    palette: {
+      type: 'dark'
+    }
+  });
   return (
-    <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
-    </div>
+    <ThemeProvider theme={ theme }>
+      <div className="App">
+        <NavBar title="NPDay" _date={ nDate } />
+        <Container>
+          <p>
+            Read through the instructions in the README.md file to build your NASA
+            app! Have fun 🚀!
+          </p>
+        </Container>
+      </div>
+    </ThemeProvider>
   );
 }
 
